667c3df4c63d2347620fed13767e3f7a80e7fc96
nguyenbanguyen <nguyenbanguyenth@gmail.com>
2017-04-02 18:43:08 +0700

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoRestaurant.DAL;
+using DemoRestaurant.Models;
+
+namespace DemoRestaurant.Controllers
+{
+    [RequireHttps]
+    public class HomeController : Controller
+    {
+        RestaurantDemoContext ResDb = new RestaurantDemoContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [ChildActionOnly]
+        public  ActionResult CategoriesMenu()
+        {
+            var categories = from s in ResDb.Category select s;
+            return PartialView("CategoriesMenu", categories.ToList());
+        }
+    }
+}
